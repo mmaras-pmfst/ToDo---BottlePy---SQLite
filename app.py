804583e3880f3fe304e90e93cce5f672d23a7e4c
mmaras-pmfst <mmaras@pmfst.hr>
2019-12-07 13:23:25 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from bottle import Bottle, run, \
+     template, debug, get, route, static_file
+
+import os, sys
+
+dirname = os.path.dirname(sys.argv[0])
+
+app = Bottle()
+debug(True)
+
+@app.route('/static/<filename:re:.*\.css>')
+def send_css(filename):
+    return static_file(filename, root=dirname+'/static/assets/css')
+
+@app.route('/static/<filename:re:.*\.css.map>')
+def send_cssmap(filename):
+    return static_file(filename, root=dirname+'/static/assets/css')
+
+@app.route('/static/<filename:re:.*\.js>')
+def send_js(filename):
+    return static_file(filename, root=dirname+'/static/assets/js')
+
+@app.route('/static/<filename:re:.*\.js.map>')
+def send_jsmap(filename):
+    return static_file(filename, root=dirname+'/static/assets/js')
+
+
+
+@app.route('/')
+def index():
+    data = {"developer_name": "PMF student",
+            "developer_organization": "PMF"}
+    return template('index', data = data)
+
+run(app, host='localhost', port = 4040)
