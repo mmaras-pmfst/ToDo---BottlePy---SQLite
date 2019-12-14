0da4fd42405ae259d590023d17a96db65474705c
dev-mirko <msisko@pmfst.hr>
2019-12-14 13:46:30 +0100

    New design

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,23 +1,10 @@
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
+const addBtn = document.querySelector('.addBtn');
+if (addBtn) {
+  addBtn.addEventListener('click', () => {
+    let value = document.querySelector('.taskInput').value;
+    if (value) {
 
-	burger.addEventListener('click', () => {
-		//Toggle Nav  
-		nav.classList.toggle('nav-active');
+    }
 
-		//Animate Links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-			}
-		});
-		//Burger Animation
-		burger.classList.toggle('toggle');
-	});
+  });
 }
-
-navSlide();
